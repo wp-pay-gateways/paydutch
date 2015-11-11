df613c45d64490199b0937ecd68ce9068ab058e4
Remco Tolsma <info@remcotolsma.nl>
2015-11-11 14:00:41 +0100

    Simplified integration class.

--- src/Integration.php
+++ src/Integration.php
@@ -1,8 +1,12 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_PayDutch_GatewayIntegration {
+class Pronamic_WP_Pay_Gateways_PayDutch_Integration {
 	public function __construct() {
-		$this->id = 'paydutch';
+		$this->id            = 'paydutch';
+		$this->name          = 'PayDutch';
+		$this->url           = 'http://www.paydutch.nl/',
+		$this->dashboard_url = 'https://www.paydutch.nl/paydutchmanager/';
+		$this->provider      = 'paydutch';
 	}
 
 	public function get_config_factory_class() {
