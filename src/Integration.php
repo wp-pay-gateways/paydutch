4abc8b05dfa7748cba0d5c6d235c8f029918367c
Remco Tolsma <info@remcotolsma.nl>
2015-11-19 14:16:16 +0100

    Extend from common abstract integration class.

--- src/Integration.php
+++ src/Integration.php
@@ -1,6 +1,6 @@
 <?php
 
-class Pronamic_WP_Pay_Gateways_PayDutch_Integration {
+class Pronamic_WP_Pay_Gateways_PayDutch_Integration extends Pronamic_WP_Pay_Gateways_AbstractIntegration {
 	public function __construct() {
 		$this->id            = 'paydutch';
 		$this->name          = 'PayDutch';
